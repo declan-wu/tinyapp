ea9e78758813288ebab8bbfc66b283fa76d4c937
Declan Wu <declan.s.wu@gmail.com>
2019-08-05 14:11:32 -0400

    update urls index ejs, so we can present urls

--- express_server.js
+++ express_server.js
@@ -7,20 +7,8 @@ app.set("view engine", "ejs");
 
 // use res.render to load up an ejs view file
 // index page
-// index page
 app.get("/", function(req, res) {
-  var drinks = [
-    { name: "Bloody Mary", drunkness: 3 },
-    { name: "Martini", drunkness: 5 },
-    { name: "Scotch", drunkness: 10 }
-  ];
-  var tagline =
-    "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
-  res.render("pages/index", {
-    drinks: drinks,
-    tagline: tagline
-  });
+  res.render("pages/index");
 });
 
 // about page
@@ -35,6 +23,11 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("pages/urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
