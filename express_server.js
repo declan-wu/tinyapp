947364a30b17efefcef8a910485d7456d27f8380
Declan Wu <declan.s.wu@gmail.com>
2019-08-06 18:49:11 -0400

    added login log out features

--- express_server.js
+++ express_server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const urlsRouter = require("./routes/urls");
+const authenticationRouter = require("./routes/authentication");
 const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/urls", urlsRouter);
+app.use("/authentication", authenticationRouter);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
