9b2cd53f166635cfe1c3098db51e9a0c5b4529d3
Declan Wu <48491890+declan-wu@users.noreply.github.com>
2019-08-09 07:23:06 -0400

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 const { isRightUser } = require("../helper");
 const users = require("../data/users");
 const router = express.Router();
